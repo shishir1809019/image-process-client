6f1e8bf39fcd83dd09073d8e734d59737232f2ca
shishir1809019 <shishirdas726@gmail.com>
2021-12-16 19:27:43 +0600

    readme file edited

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # image-process-client
 
-live site:
-server-side-link:
+live site:https:https://image-process.netlify.app/
+server-side-link:https://github.com/shishir1809019/image-process-server
 
 - this project is for upload image in the data base with some image info.
 - image have saved in database with base64 form
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,12 +6,9 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -68,7 +65,7 @@ function Dashboard(props) {
     formData.append("template", template);
     formData.append("image", image);
 
-    fetch("http://localhost:5000/imageWithData", {
+    fetch("https://blooming-waters-39053.herokuapp.com/imageWithData", {
       method: "POST",
       body: formData,
     })
@@ -86,7 +83,7 @@ function Dashboard(props) {
 
   const [imagesWithData, setImagesWithData] = React.useState([]);
   React.useEffect(() => {
-    fetch("http://localhost:5000/imageWithData")
+    fetch("https://blooming-waters-39053.herokuapp.com/imageWithData")
       .then((res) => res.json())
       .then((data) => setImagesWithData(data));
   }, [isLoading]);
